36486c58f2177df4745033a4f8654528fff5eeaf
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-06-04 00:42:31 +0200

    Corrección de tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     extend: {
       colors: {
         'txt-orange': 'rgb(245, 157, 35)',
-        'title-blue': 'rgb(61, 131, 164)',
-        'label-blue': 'rgb(0, 89, 152)',
         'border-orange': 'rgb(218,165,32)',
+        'title-blue': 'rgb(61, 131, 164)',
+        'label-blue': 'rgb(0, 89, 152)'
       },
       fontFamily: {
         sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 
