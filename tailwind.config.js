e77242a72ac2de162e2b5a4f8a3be067b6cca04a
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-06-05 00:29:39 +0200

    Se añaden imágenes que faltaban y se rediseña el footer con Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'txt-orange': 'rgb(245, 157, 35)',
+        /* 'txt-orange': 'rgb(245, 157, 35)', */
         'border-orange': 'rgb(218,165,32)',
         'title-blue': 'rgb(61, 131, 164)',
-        'label-blue': 'rgb(0, 89, 152)'
+        'label-blue': 'rgb(0, 89, 152)',
+        'footer-links': 'rgb(13,110,253)',
+        'footer-links-hover': 'rgb(10,88,202)',
       },
       fontFamily: {
         sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 
