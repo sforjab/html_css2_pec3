7860f0255a8b20e1a7a00f49bfbfffca585f8353
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-06-06 00:57:41 +0200

    Se modifican estilos de participantes y se diseña responsive de header y footer, así como configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        /* 'txt-orange': 'rgb(245, 157, 35)', */
+        'txt-orange': 'rgb(245, 157, 35)',
         'border-orange': 'rgb(218,165,32)',
         'title-blue': 'rgb(61, 131, 164)',
         'label-blue': 'rgb(0, 89, 152)',
@@ -15,6 +15,12 @@ module.exports = {
         sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 
                '"Helvetica Neue"', 'Arial', 'sans-serif', 
                '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"'],
+      },
+      fontSize: {
+        'header-title': '2rem',
+      },
+      fontWeight: {
+        'header-weight': 500,
       },
     },
   },
